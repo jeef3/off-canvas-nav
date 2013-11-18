c8e00157f4b480923f34a7ad36b76da1d2973c30
Jeff Knaggs <mail@jeef3.com>
2013-11-18 13:55:44 +1300

    Fixed touch/mouse event handling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,12 @@
 (function () {
-    $('.filter-panel, .main').on('click', function () {
-        $('.pusher').toggleClass('is-pushed');
-    });
+    var $pusher = $('.pusher');
+
+    $('.pusher')
+        .on('touchend', function (e) {
+            $pusher.toggleClass('is-pushed');
+            e.preventDefault();
+        })
+        .on('click', function (e) {
+            $pusher.toggleClass('is-pushed');
+        })
 })();
