cde995df82b5e023fcb2db53596cc5d5e603f9e5
Jeff Knaggs <mail@jeef3.com>
2013-11-18 14:16:24 +1300

    Fixed touch/drag events

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,19 @@
 (function () {
-    var $pusher = $('.pusher');
+    var $pusher = $('.pusher'),
+        dragging = false;
 
     $('.pusher')
+        .on('touchmove', function (e) {
+            dragging = true;
+        })
+        .on('touchstart', function (e) {
+            dragging = false;
+        })
         .on('touchend', function (e) {
+            if (dragging) {
+                return;
+            }
+
             $pusher.toggleClass('is-pushed');
             e.preventDefault();
         })
